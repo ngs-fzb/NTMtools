99e908c0379c207e99e4103a2db96b7de3fcec6a
mdiricks <diricks_margo@hotmail.com>
2022-07-13 15:32:25 +0200

    Add files via upload

---
+++ scripts/starter_Assemble_usingShovill_Mab.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+
+###Usage###
+# bash $PathToScript/Scriptname.sh
+
+###Author###
+echo "hello World, this script was written by Margo Diricks!"
+
+###Function###
+echo "This script creates assemblies from illumina fastQ files for M. abscessus"
+echo "This script calls the shovill pipeline (https://github.com/tseemann/shovill) and summarises the output (assembly statistics) into a text file."
+
+###Required packages###
+echo "You need to install Shovill (Tseemann), e.g. using conda"
+echo "Don´t forget to activate the conda environment where you have installed these tools before running this script!"
+echo "Don´t forget to change the required parameters in this script!"
+	
+###!!!PARAMATERS THAT NEED TO BE CHANGED!!!###
+###Input and output files###
+#Input Type: Paired-end raw fastQ files (with sufficient coverage!)
+PATH_input=""
+Fw="R1" #Change if your fastQ files are not SampleName_R1.fastq.gz and SampleName_R2.fastq.gz
+Rv="R2" #Change if your fastQ files are not SampleName_R1.fastq.gz and SampleName_R2.fastq.gz
+#Output Type: FastA assemblies
+PATH_output=""
+
+###OPTIONAL PARAMATERS THAT YOU CAN CHANGE###
+###Shovill Parameters###
+#Expected genome size
+egs="5.1M"
+#Downsampling to
+cov=100
+#Assembler (choose skesa velvet megahit or spades)
+ass="skesa"
+#Resources
+cpu=0 #This depends on your hardware configuration!
+
+###Estimated time### 
+#Between 4 and 20 min per sample depending on coverage and assembler used
+
+################################################################################################################################################################
+mkdir -p $PATH_output
+mkdir $PATH_output/FinalAssemblies
+rm $PATH_output/FinalAssemblies/Failed.txt
+#Loop through all the raw fastQ files
+for fastq in $PATH_input/*_$Fw.fastq.gz 
+do
+	SampleName=$(basename $fastq| cut -d '_' -f 1)
+	if [ -f "$PATH_output/FinalAssemblies/$SampleName.fasta" ]
+	then
+		 echo "Sample was already analysed" >> $PATH_output/FinalAssemblies/AlreadyAnalyzed.txt
+	else
+		shovill --R1 $fastq --R2 $(echo $fastq | sed "s/$Fw.fastq.gz$/$Rv.fastq.gz/1") --outdir $PATH_output/$SampleName --gsize $egs --depth $cov --trim --assembler $ass 
+#Move the final assembly to a seperate folder and rename file
+		if [ -f "$PATH_output/$SampleName/contigs.fa" ]
+		then
+			mv $PATH_output/$SampleName/contigs.fa $PATH_output/FinalAssemblies/$SampleName"_Assembly_Shovill_"$ass.fasta
+		else
+			echo $SampleName >> $PATH_output/FinalAssemblies/Failed.txt
+		fi
+	fi
+done
+
+# Create summary file
+rm $PATH_output/"shovillOutput_summary.txt"
+#Make header of the summary file 
+echo -e "Sample\tEstimated_depth(x)\tRead_max_len\tRead_avg_len\tRead_min_len\tSurviving_Read_Pairs_trimmomatic_perc\tFw_surviving_trimmomatic_perc\tRv_surviving_trimmomatic_perc\tDropped_trimmomatic_perc\tFlash_CombinedPairs_perc\tWalltime\tContigAmount\tMinContigLength\tAssemblyLength\tAssemblyRel%("$egs")\tAssembler\tREC\tShovillVersion\tFullCode" >> $PATH_output/shovillOutput_summary.txt
+
+#Loop through all the generated log files and extract necessary info
+for log in $PATH_output/*/shovill.log
+do
+	#ReadStats
+	SampleName=$(basename $(dirname $log))
+	max_len=$(grep 'max_len' $log| cut -d '=' -f 2)
+	avg_len=$(grep 'avg_len' $log| cut -d '=' -f 2)
+	min_len=$(grep 'min_len' $log| cut -d '=' -f 2)
+	EstSeqDepth=$(grep 'Estimated.sequencing.depth' $log| cut -d ':' -f 2 | sed 's/\s//' | sed 's/x//')
+	Flash_CombinedPairs_perc=$(grep -o 'Percent.combined.*%' $log| cut -d ':' -f 2 | sed 's/\s//' | sed 's/%//')
+	Surviving_Read_Pairs_trimmomatic_perc=$(grep -Po 'Both.Surviving.*?%' $log| grep -Po '.....%' | sed 's/%//') #-P means that you want to use non-greedy expression .*? which means any character multiple times until the first character you put after this expression; -o means output only matching part, not complete line
+	Fw_surviving_trimmomatic_perc=$(grep -P -o 'Forward.Only.Surviving.*?%' $log| grep -Po '....%' | sed 's/%//')
+	Rv_surviving_trimmomatic_perc=$(grep -P -o 'Reverse.Only.Surviving.*?%' $log| grep -Po '....%' | sed 's/%//')
+	Dropped_trimmomatic_perc=$(grep -Po 'Dropped.*?%' $log| grep -Po '....%' | sed 's/%//')
+	Walltime=$(grep 'Walltime' $log| cut -d ':' -f 2 | sed 's/\s//')
+	Contig_amount=$(grep 'It.contains' $log| cut -d ' ' -f 4)
+	min_contig_length=$(grep -P -o '\(min=.*?\)' $log| cut -d '=' -f 2 | sed -e 's/)//')
+	Assembly_length=$(grep -P -o 'Assembly.is.*?,' $log| cut -d ' ' -f 3 | sed 's/,//')
+	Assembly_rel=$(grep 'Assembly.is' $log| grep -Po '\(.*?\)'| sed -e 's/)//' | sed -e 's/(//' | sed -e 's/%//')
+	GC=$(awk '!/^>/{gc+=gsub(/[gGcC]/,""); at+=gsub(/[aAtT]/,"");} END{ printf "%.1f", (gc*100)/(gc+at) }' $PATH_output/FinalAssemblies/$SampleName"_Assembly_Shovill_"$ass.fasta)
+	Assembler=$(grep -oP -m 1 'assembler\s.*' $log | cut -d ' ' -f 2) #-P to use Perl regular expression, -o to output only match, \s to match space, \K to omit the match, .* to match rest of the string(s)
+	REC="Unknown" #Read error correction option from shovill
+	if grep -Fxq 'noreadcorr' $log; then REC="no"; else REC="Yes"; fi
+	Shovill_v=$(grep 'This.is.shovill' $log | cut -d ' ' -f 5)
+	FullCode=$(grep 'You.ran' $log)
+	paste --delimiter='\t'  <(echo $SampleName)	<(echo $EstSeqDepth) <(echo $max_len) <(echo $avg_len) <(echo $min_len) <(echo $Surviving_Read_Pairs_trimmomatic_perc) <(echo $Fw_surviving_trimmomatic_perc) <(echo $Rv_surviving_trimmomatic_perc) <(echo $Dropped_trimmomatic_perc) <(echo $Flash_CombinedPairs_perc) <(echo $Walltime) <(echo $Contig_amount) <(echo $min_contig_length) <(echo $Assembly_length) <(echo $Assembly_rel) <(echo $Assembler) <(echo $REC) <(echo "v"$Shovill_v) <(echo $FullCode)>> $PATH_output/shovillOutput_summary.txt
+done
+
+exit 0
+################################################################################################################################################################
+#QUICK START: shovill --outdir out --R1 test/R1.fq.gz --R2 test/R2.fq.gz
+
+#MAIN STEPS
+# Estimate genome size and read length from reads (unless --gsize provided)
+# Reduce FASTQ files to a sensible depth (default --depth 100)
+# Trim adapters from reads (with --trim only)
+# Conservatively correct sequencing errors in reads
+# Pre-overlap ("stitch") paired-end reads
+# Assemble with SPAdes/SKESA/Megahit with modified kmer range and PE + long SE reads
+# Correct minor assembly errors by mapping reads back to contigs
+# Remove contigs that are too short, too low coverage, or pure homopolymers
+# Produce final FASTA with nicer names and parseable annotations
+
+# SYNOPSIS
+  # De novo assembly pipeline for Illumina paired reads
+# USAGE
+  # shovill [options] --outdir DIR --R1 R1.fq.gz --R2 R2.fq.gz
+# GENERAL
+  # --help          This help
+  # --version       Print version and exit
+  # --check         Check dependencies are installed
+# INPUT
+  # --R1 XXX        Read 1 FASTQ (default: '')
+  # --R2 XXX        Read 2 FASTQ (default: '')
+  # --depth N       Sub-sample --R1/--R2 to this depth. Disable with --depth 0 (default: 150)
+  # --gsize XXX     Estimated genome size eg. 3.2M <blank=AUTODETECT> (default: '')
+# OUTPUT
+  # --outdir XXX    Output folder (default: '')
+  # --force         Force overwite of existing output folder (default: OFF)
+  # --minlen N      Minimum contig length <0=AUTO> (default: 0)
+  # --mincov n.nn   Minimum contig coverage <0=AUTO> (default: 2)
+  # --namefmt XXX   Format of contig FASTA IDs in 'printf' style (default: 'contig%05d')
+  # --keepfiles     Keep intermediate files (default: OFF)
+# RESOURCES
+  # --tmpdir XXX    Fast temporary directory (default: '/tmp/tseemann')
+  # --cpus N        Number of CPUs to use (0=ALL) (default: 8)
+  # --ram n.nn      Try to keep RAM usage below this many GB (default: 16)
+# ASSEMBLER
+  # --assembler XXX Assembler: skesa velvet megahit spades (default: 'spades')
+  # --opts XXX      Extra assembler options in quotes eg. spades: "--untrusted-contigs locus.fna" ... (default: '')
+  # --kmers XXX     K-mers to use <blank=AUTO> (default: '')
+# MODULES
+  # --trim          Enable adaptor trimming (default: OFF)
+  # --noreadcorr    Disable read error correction (default: OFF)
+  # --nostitch      Disable read stitching (default: OFF)
+  # --nocorr        Disable post-assembly correction (default: OFF)
+  
+#--trim option:
+#[trimmomatic]  -threads 8 -phred33 R1.sub.fq.gz R2.sub.fq.gz R1.fq.gz /dev/null R2.fq.gz /dev/null ILLUMINACLIP:/home/mdiricks/bin/miniconda2/db/trimmomatic.fa:1:30:11 LEADING:3 TRAILING:3 MINLEN:30 TOPHRED33
+
+# This will perform the following in this order
+# Remove Illumina adapters provided in the trimmomatic.fa file (provided). Initially
+# Trimmomatic will look for seed matches (16 bases) allowing maximally 1
+# mismatch. These seeds will be extended and clipped if in the case of paired end
+# reads a score of 30 is reached (about 50 bases), or in the case of single ended reads a
+# score of 11, (about 17 bases).
+# Remove leading low quality or N bases (below quality 3)
+# Remove trailing low quality or N bases (below quality 3)
+# Drop reads which are less than 30 bases long after these steps
---
+++ scripts/starter_SRA-Download_usingFasterq-dump.sh
@@ -0,0 +1,169 @@
+#! /bin/bash
+
+###Usage###
+# bash $PathToScript/NameScript.sh
+
+###Author###
+echo "hello World, this script was written by Margo Diricks (mdiricks@fz-borstel.de)!"
+
+###Function###
+echo "Running this script will download fastQ files from SRA, very fast!"
+
+###Required packages###
+echo "You need to install fasterq-dump and pigz, e.g. using conda"
+echo "Don´t forget to activate the conda environment where you have installed these tools before running this script!"
+echo "Don´t forget to change the required parameters in this script!"
+
+###!!!PARAMATERS THAT NEED TO BE CHANGED!!!###
+
+###Output folder###
+PATH_output="$HOME/auto/Thecus_SeqData/Hinfluenzae_mdiricks/SRA_samples/Test8" #Directory where fastQ files will be stored
+TempDir="$HOME/auto/Thecus_Homes/Thecus_mdiricks/Tmp" #It is important that this temporary directory has enough space, otherwise the tool wil fail!! This temporary directory will use approximately up to 10 times the size of the final output-file. this directory and its content will be deleted.
+
+###Required FastQ files###
+#Enter the accession numbers of the datasets you want to download (SRR or ERR numbers). Each accession number should be on a seperate line (you can e.g. easily copy paste a column from excel here)
+group_toDownload=(
+SRR12805684
+SRR12805671
+)
+
+###OPTIONAL PARAMATERS THAT YOU CAN CHANGE###
+
+cpu=16 #Resources to be used (max. amount depends on your hardware configuration)
+
+###EstimatedTime###
+#This script takes less than 2 min for 2 Haemophilus samples with compressed file sizes of about 100 MB (SRR12805684 and SRR12805671) using 16 threads
+
+#No changes required below this point
+###############################################################################CODE#################################################################################
+###Pre-processing###
+mkdir $PATH_output
+rm $PATH_output/Info.txt
+
+group_toDownload2=()
+#Check if sample was already downloaded
+for id in "${group_toDownload[@]}"
+do 
+	if [[ -s $PATH_output/$id"_1.fastq.gz" ]] || [[ -s $PATH_output/$id"_1.fastq" ]] || [[ -s $PATH_output/$id".fastq" ]]
+	then
+		echo "Sample was already downloaded"
+	else 
+		group_toDownload2+=( $id )
+	fi
+done
+
+###Create info file###
+i=0
+SampleCount=${#group_toDownload2[@]}
+echo $SampleCount
+echo "SampleCount="$SampleCount > $PATH_output/Info.txt
+echo "FirstSample="${group_toDownload2[0]} >> $PATH_output/Info.txt
+echo "LastSample="${group_toDownload2[-1]} >> $PATH_output/Info.txt
+
+###Actual command###
+for id in "${group_toDownload2[@]}";
+do echo "Processing $id"; 
+	fasterq-dump --outdir $PATH_output --temp $TempDir --threads $cpu --split-files $id 
+done
+
+###Post-processing###
+cache-mgr.2.8.2 -c #This is not required if you configure SRA tools in such a way that no caching occurs!
+
+rm $PATH_output/Failed.txt
+rm $PATH_output/downloaded.txt
+for id in "${group_toDownload[@]}"
+do
+	if [[ -s $PATH_output/$id"_4.fastq" ]]
+	then
+		echo $id "	was downloaded as more than 2 files" >> $PATH_output/Failed.txt
+	elif [[ -s $PATH_output/$id"_1.fastq" ]] && [[ -s $PATH_output/$id"_2.fastq" ]] && [[ -s $PATH_output/$id".fastq" ]]
+	then
+		echo $id "	was downloaded as paired-end files and unpaired file" >> $PATH_output/downloaded.txt
+	elif [[ -s $PATH_output/$id"_1.fastq" ]] && [[ -s $PATH_output/$id"_2.fastq" ]]
+	then
+		echo $id "	was downloaded as 2 paired-end files" >> $PATH_output/downloaded.txt
+	elif [[ -s $PATH_output/$id".fastq" ]]
+	then
+		echo $id "	was downloaded as 1 file" >> $PATH_output/Failed.txt
+	else 
+		echo $id >> $PATH_output/Failed.txt
+	fi
+done
+
+####################################################################CODE THAT MIGHT BE USED IN ADDITION#################################################################################
+
+cd $PATH_output
+#mmv "*_*.fastq" "#1_SRA_Download_Date_xbp_R#2.fastq"
+#gzip *.fastq # much slower!
+pigz *.fastq
+
+echo "Script Finished!"
+
+
+#####################################################################HELP#########################################################
+
+# Usage: fasterq-dump [ options ] [ accessions(s)... ]
+
+# Parameters:
+
+  # accessions(s)                    list of accessions to process
+
+
+# Options:
+
+  # -o|--outfile <path>              full path of outputfile (overrides usage
+                                     # of current directory and given accession)
+  # -O|--outdir <path>               path for outputfile (overrides usage of
+                                     # current directory, but uses given
+                                     # accession)
+  # -b|--bufsize <size>              size of file-buffer (dflt=1MB, takes
+                                     # number or number and unit where unit is
+                                     # one of (K|M|G) case-insensitive)
+  # -c|--curcache <size>             size of cursor-cache (dflt=10MB, takes
+                                     # number or number and unit where unit is
+                                     # one of (K|M|G) case-insensitive)
+  # -m|--mem <size>                  memory limit for sorting (dflt=100MB,
+                                     # takes number or number and unit where
+                                     # unit is one of (K|M|G) case-insensitive)
+  # -t|--temp <path>                 path to directory for temp. files
+                                     # (dflt=current dir.)
+  # -e|--threads <count>             how many threads to use (dflt=6)
+  # -p|--progress                    show progress (not possible if stdout used)
+  # -x|--details                     print details of all options selected
+  # -s|--split-spot                  split spots into reads
+  # -S|--split-files                 write reads into different files
+  # -3|--split-3                     writes single reads into special file
+     # --concatenate-reads           writes whole spots into one file
+  # -Z|--stdout                      print output to stdout
+  # -f|--force                       force overwrite of existing file(s)
+  # -N|--rowid-as-name               use rowid as name (avoids using the name
+                                     # column)
+     # --skip-technical              skip technical reads
+     # --include-technical           explicitly include technical reads
+  # -P|--print-read-nr               include read-number in defline
+  # -M|--min-read-len <count>        filter by sequence-lenght
+     # --table <name>                which seq-table to use in case of pacbio
+     # --strict                      terminate on invalid read
+  # -B|--bases <bases>               filter output by matching against given
+                                     # bases
+  # -A|--append                      append to output-file, instead of
+                                     # overwriting it
+     # --ngc <path>                  <path> to ngc file
+     # --perm <path>                 <path> to permission file
+     # --location <location>         location in cloud
+     # --cart <path>                 <path> to cart file
+  # -V|--version                     Display the version of the program
+  # -v|--verbose                     Increase the verbosity of the program
+                                     # status messages. Use multiple times for
+                                     # more verbosity.
+  # -L|--log-level <level>           Logging level as number or enum string.
+                                     # One of
+                                     # (fatal|sys|int|err|warn|info|debug) or
+                                     # (0-6) Current/default is warn
+     # --option-file file            Read more options and parameters from the
+                                     # file.
+  # -h|--help                        print this message
+
+# fasterq-dump version 2.11.0
+
+exit
